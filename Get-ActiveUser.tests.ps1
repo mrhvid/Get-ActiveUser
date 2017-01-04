1f3aaa30f7dd9dcf2ec48a1297d34c209e3ceb2b
mrhvid <git@1d.dk>
2017-01-04 17:54:52 +0100

    demo tests

--- Get-ActiveUser.tests.ps1
+++ Get-ActiveUser.tests.ps1
@@ -1,1 +1,26 @@
 # tests 
+
+
+Describe "This is the description" {
+    Context "This is the sub-description" {
+    }
+}
+
+
+Describe "My scenario" {
+
+    BeforeEach {
+        "I run first"
+    }
+
+    Context "My Context" {
+
+        BeforeEach { "I run second" }
+
+        It "Demonstrates something" {
+            "I run third" | Should BeOfType System.String
+        }
+    
+    }
+
+}
