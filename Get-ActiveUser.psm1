c53ac0fe32434d254c9861734a73c4af703386be
Ian Mott <mottianr@gmail.com>
2018-12-29 13:17:50 -0500

    added option for credentials for WMI connections, additional logging, param aliases, and reformated function information

--- Get-ActiveUser.psm1
+++ Get-ActiveUser.psm1
@@ -1,20 +1,46 @@
-﻿<#
-.Synopsis
-   Retrive list of active users on windows machine
-.DESCRIPTION
-   Uses WMI, CIM or Query.exe
-   
-   This module was created with a powershell.org blogpost in mind
-   http://powershell.org/wp/2015/08/28/list-users-logged-on-to-your-machines/
-   Created by Jonas Sommer Nielsen
-
-.ExternalHelp Get-ActiveUser-Help.xml
-
-.Example
-    test  this is a test of git
-#>
-function Get-ActiveUser
+﻿function Get-ActiveUser
 {
+    <#
+    .SYNOPSIS
+		Retrive list of active users on windows machine
+	
+	.DESCRIPTION
+		Uses WMI, CIM or Query.exe
+
+        This module was created with a powershell.org blogpost in mind
+        http://powershell.org/wp/2015/08/28/list-users-logged-on-to-your-machines/
+        Created by Jonas Sommer Nielsen
+	
+	.PARAMETER ComputerName / CN / IP / Hostname
+		Optional: Specifies a remote computer to target
+	
+	.PARAMETER Method
+		Optional: Specifies the method to retrieve logged on users. Query, CIM, WMI
+	
+	.PARAMETER Credential
+		Optional: Specifies alternative credentials to use for the WMI connection
+	
+	.EXAMPLE
+        Get-ActiveUser
+        Retrieves all users currently logged into the local machine
+    
+    .EXAMPLE
+        Get-ActiveUser -ComputerName TestComputer -Method CIM
+        Retrieves all users currently logged into the remote machine "TestComputer" using CIM
+
+    .EXAMPLE
+        Get-ActiveUser -ComputerName TestComputer -Method WMI -Credential (Get-Credential)
+        Retrieves all users currently logged into the remote machine "TestComputer" using WMI.
+        This will prompt for credentials to authenticate the connection.
+
+    .ExternalHelp 
+        https://github.com/mrhvid/Get-ActiveUser
+        
+	.NOTES
+        Author: Jonas Sommer Nielsen
+        Revised: Ian Mott
+    #> 
+
     [CmdletBinding(DefaultParameterSetName='Standard Parameters', 
                 SupportsShouldProcess=$false, 
                 PositionalBinding=$false,
@@ -25,27 +51,70 @@ function Get-ActiveUser
     Param
     (
         # Computer name, IP, Hostname
-        [Parameter(Mandatory=$true,
+        [Parameter(Mandatory=$false,
                    ValueFromPipelineByPropertyName=$true,
+                   HelpMessage="Default set to localhost",
                    Position=0)]
+        [Alias("CN","IP","Hostname")]
         [String]
-        $ComputerName,
+        $ComputerName = $ENV:COMPUTERNAME,
 
         # Choose method, WMI, CIM or Query
-        [Parameter(Mandatory=$true,
+        [Parameter(Mandatory=$false,
             ValueFromPipelineByPropertyName=$true,
+            HelpMessage="Default set to WMI",
             Position=1)]
         [ValidateSet('WMI','CIM','Query')]
         [String]
-        $Method
+        $Method = "WMI",
+
+        # Specify Credentials for the remote WMI/CIM queries
+        [Parameter(Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            HelpMessage="This is only required for WMI connections. Try the Query or CIM method?",
+            Position=2)]
+        [pscredential]
+        $Credential
     )
 
     Begin
     {
-        #test
+        Write-Verbose -Message "VERBOSE: Starting Begin"
+
+        $Params = @{}
+
+        if ($ComputerName -notin ($ENV:COMPUTERNAME,"localhost", "127.0.0.1")) {
+            if ($Method -in ("WMI","CIM")) {
+                $Params.Add("ComputerName",$ComputerName)
+                
+                if ($Credential -and $Method -eq "WMI") {
+                    $Params.Add("Credential",$Credential)
+                }
+            }
+
+            if (Test-Connection -ComputerName $ComputerName -Count 1 -Quiet) {
+                Write-Verbose -Message "VERBOSE: Confirmed $ComputerName is reachable by ping"
+
+                if (Test-WSMan @Params -ErrorAction SilentlyContinue -ErrorVariable error_WSMan) {
+                    Write-Verbose -Message "VERBOSE: Successfully connected with WSMan"
+                } else {
+                    Write-Error -Message "ERROR: Failed to connect with WSMan. ErrorMessage: $error_WSMan" -RecommendedAction Stop
+                }
+                
+            } else {
+                Write-Error -Message "ERROR: Could not reach $ComputerName by ping. Confirm the computer is reachable." -RecommendedAction Stop
+            }
+            
+        } else {
+            Write-Verbose -Message "VERBOSE: ComputerName not set to a remote machine. No need to check for connectivity."
+        }
+    
+        Write-Verbose -Message "VERBOSE: Ending Begin"
     }
     Process
     {
+        Write-Verbose -Message "VERBOSE: Starting Process"
+
         Write-Verbose "$Method selected as method"
 
         switch ($Method)
@@ -53,8 +122,9 @@ function Get-ActiveUser
             'WMI' 
             {
                 Write-Verbose "Contacting $ComputerName via WMI"
-          
-                $WMI = (Get-WmiObject Win32_LoggedOnUser).Antecedent
+        
+                $WMI = (Get-WmiObject Win32_LoggedOnUser @Params).Antecedent
+
                 $ActiveUsers = @()
                 foreach($User in $WMI) {
                     $StartOfUsername = $User.LastIndexOf('=') + 2
@@ -67,7 +137,7 @@ function Get-ActiveUser
             'CIM' 
             {
                 Write-Verbose "Contacting $ComputerName via CIM"
-                $ActiveUsers = (Get-CimInstance Win32_LoggedOnUser -ComputerName $ComputerName).antecedent.name | Select-Object -Unique
+                $ActiveUsers = (Get-CimInstance Win32_LoggedOnUser @Params).antecedent.name | Select-Object -Unique
 
             }
             'Query' 
@@ -86,6 +156,12 @@ function Get-ActiveUser
 
         }
 
+        Write-Verbose -Message "VERBOSE: Ending process"
+    }
+    End
+    {
+        Write-Verbose -Message "VERBOSE: Starting End"
+
         # Create nice output format
         $UsersComputersToOutput = @()
         foreach($User in $ActiveUsers) {
@@ -94,8 +170,7 @@ function Get-ActiveUser
 
         # output data
         $UsersComputersToOutput
-    }
-    End
-    {
+        
+        Write-Verbose -Message "VERBOSE: Ending End"
     }
 }
